5eaffd3ccabe1a3ace2f3cb05992d72960457499
bartek86 <bartek.lesniak86@gmail.com>
2015-01-12 20:38:16 +0000

    new posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,23 +9,32 @@ class PostsController < ApplicationController
   end
   
   def new
-    
+    @post = Post.new
   end
   
   def create
+    @post = Post.new(posts_params)
     
+    if @post.save
+      redirect_to posts_path, :notice => "Post saved"
+    else
+      render "new"
+    end
   end
   
   def edit
-    
   end
   
   def update
-    
   end
   
   def destroy
-    
+  end
+  
+  private 
+  
+  def posts_params
+    params.require(:post).permit(:title, :content)
   end
   
 end
